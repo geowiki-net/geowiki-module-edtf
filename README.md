23d79cca7a331f573a40ea0efba0725b2178b897
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2024-01-22 22:25:53 +0100

    Add edtf_max; check errors; documentation

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # geowiki-module-edtf
 Support for EDTF (Extended date/time format) filters
+
+# API
+Exposes the following twig filters:
+
+* edtf_min: returns the earliest UNIX timestamp (returns int or null if the value was not valid).
+* edtf_max: returns the latest UNIX timestamp (returns int or null if the value was not valid).
--- src/edtf.js
+++ src/edtf.js
@@ -2,5 +2,17 @@ const Twig = require('twig')
 const edtf = require('edtf')
 
 Twig.extendFilter('edtf_min', value => {
-  return edtf(value).min
+  try {
+    return edtf(value).min
+  } catch (e) {
+    return null
+  }
+})
+
+Twig.extendFilter('edtf_max', value => {
+  try {
+    return edtf(value).max
+  } catch (e) {
+    return null
+  }
 })

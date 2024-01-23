b69d88a207b736785f5d021ac74e7aecae6ca8dc
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2024-01-23 17:26:09 +0100

    Convert to local timezone

--- src/edtf.js
+++ src/edtf.js
@@ -3,7 +3,7 @@ import edtf from 'edtf'
 
 Twig.extendFilter('edtf_min', value => {
   try {
-    return Math.ceil(edtf(value).min / 1000)
+    return timezoneShift(Math.ceil(edtf(value).min / 1000))
   } catch (e) {
     return null
   }
@@ -11,8 +11,13 @@ Twig.extendFilter('edtf_min', value => {
 
 Twig.extendFilter('edtf_max', value => {
   try {
-    return Math.floor(edtf(value).max / 1000)
+    return timezoneShift(Math.floor(edtf(value).max / 1000))
   } catch (e) {
     return null
   }
 })
+
+function timezoneShift (value) {
+  const offset = new Date(value * 1000).getTimezoneOffset()
+  return value + offset * 60
+}
